9d905a336c58fab2a9a23aa28a96552609044d8f
19018500-S-Marcus-Abijay <68017207+19018500-S-Marcus-Abijay@users.noreply.github.com>
2021-06-27 17:33:15 +0800

    updated

--- src/test/java/com/edurekademo/utilities/TestLogger.java
+++ src/test/java/com/edurekademo/utilities/TestLogger.java
@@ -36,7 +36,6 @@ public class TestLogger {
                 LOG.info(s);
                 assert(s.contains("/ by zero"));  
         }
-	
 
         @Test
         public void testGetErrorMessage3() {
